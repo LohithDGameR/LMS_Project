6ea44882b7e8917506f827d55cdbcc5ae10594a5
Lohith <kallapallilohith@gmail.com>
2025-05-16 15:47:25 +0530

    corected wh and db

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 // Connect to the MongoDB database
 const ConnectDB = async () => {
-  mongoose.connection.on("connected", () => console.log("Database Connected Successfully"));
+  mongoose.connection.on("connected", () =>
+    console.log("Database Connected Successfully")
+  );
 
   await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
 };
--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -31,7 +31,6 @@ export const clerkWebhooks = async (req, res) => {
 
       case "user.updated": {
         const userData = {
-          _id: data.id,
           email: data.email_address[0].email_address,
           name: data.first_name + " " + data.last_name,
           imagUrl: data.image_url,
