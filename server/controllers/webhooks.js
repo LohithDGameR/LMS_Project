d4e61581368e01bd4f8a7f1130e6eb834b3b62df
Lohith <kallapallilohith@gmail.com>
2025-05-16 16:58:24 +0530

    correct webhooks

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -21,7 +21,7 @@ export const clerkWebhooks = async (req, res) => {
           _id: data.id,
           email: data.email_addresses[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imagUrl: data.image_url,
+          imageUrl: data.image_url,
         };
 
         await User.create(userData);
@@ -33,7 +33,7 @@ export const clerkWebhooks = async (req, res) => {
         const userData = {
           email: data.email_address[0].email_address,
           name: data.first_name + " " + data.last_name,
-          imagUrl: data.image_url,
+          imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, userData);
         res.json({});
